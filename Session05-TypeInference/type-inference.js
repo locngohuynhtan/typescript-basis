39c58b33ae881b07907bc0cf5186c2e44189502d
Loc Ngo-Huynh-Tan <locngohuynhtan@gmail.com>
2025-07-07 23:11:51 +0700

    feat: Session05-TypeInference

---
+++ Session05-TypeInference/type-inference.js
@@ -0,0 +1,3 @@
+let count = 10;
+count = `Loc`;
+console.log(`>>> Check count = `, count);
---
+++ Session05-TypeInference/type-inference.ts
@@ -0,0 +1,8 @@
+let countSession05: string = `Loc`;
+
+console.log(`>>> Check countSession05 = `, countSession05);
+
+let namesSession05 = [`Loc`, `Ngo`, `Huynh`, 10];
+namesSession05.push(10);
+
+console.log(`>>> Check namesSession05 = `, namesSession05);
