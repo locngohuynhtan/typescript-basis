14d12a34c1eba6a268beb2b8349d2ce495b637d4
Loc Ngo-Huynh-Tan <locngohuynhtan@gmail.com>
2025-07-07 22:46:51 +0700

    feat: Session03-Types

---
+++ Session03-Types/types.js
@@ -0,0 +1,4 @@
+var name = "Loc Ngo-Huynh-Tan";
+
+console.log("length = ", name.length);
+console.log("upPerCase = ", name.toUpperCase());
---
+++ Session03-Types/types.ts
@@ -0,0 +1,4 @@
+const nameSession03 = `Loc Ngo-Huynh-Tan`;
+
+console.log(`length = `, nameSession03.length);
+console.log(`upPerCase = `, nameSession03.toUpperCase());
