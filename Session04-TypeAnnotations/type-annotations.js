f39e8e27010a2108a31c1b9928828fa063c337dc
Loc Ngo-Huynh-Tan <locngohuynhtan@gmail.com>
2025-07-07 22:58:14 +0700

    feat: Session04-TypeAnnotations

---
+++ Session04-TypeAnnotations/type-annotations.js
@@ -0,0 +1,7 @@
+let count = 1;
+count = `name`;
+console.log(`>>> Check count: `, count);
+
+let names = [`Loc`, `Ngo`, `Huynh`, `Tan`];
+names.push(25);
+console.log(`>>> Check names: `, names);
---
+++ Session04-TypeAnnotations/type-annotations.ts
@@ -0,0 +1,9 @@
+let countSession04: number;
+
+countSession04 = 10;
+
+console.log(`>>> Check countSession04: `, countSession04);
+
+let namesSession04: string[] = [`Loc`, `Ngo`, `Huynh`, `Tan`];
+namesSession04.push(`10`);
+console.log(`>>> Check namesSession04: `, namesSession04);
