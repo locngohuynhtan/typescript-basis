b5cffe2dd08f083b4b3383db7be4bccb64acdd73
Loc Ngo-Huynh-Tan <locngohuynhtan@gmail.com>
2025-07-07 23:35:31 +0700

    feat: Session06-NumberType

---
+++ Session06-NumberType/number-type.ts
@@ -0,0 +1,7 @@
+let numberSession06: number = 9.6;
+numberSession06 = 9999;
+console.log(`>>> Check numberSession06 = `, numberSession06);
+
+// bigint
+let bigNumber: bigint = 9999n;
+console.log(`>>> Check bigNumber = `, bigNumber); // 9999n, value is 9999, type is bigint
