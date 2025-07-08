2a8441404c7cb20293176111de90db1b614ff876
Loc Ngo-Huynh-Tan <locngohuynhtan@gmail.com>
2025-07-08 07:27:12 +0700

    feat: Session08-BooleanType

---
+++ Session08-BooleanType/boolean-type.ts
@@ -0,0 +1,7 @@
+let statusSession08: boolean = false;
+let checkSession08: boolean = true;
+let proSession08: boolean = Boolean("Loc Ngo-Huynh-Tan");
+let pro1Session08: boolean = Boolean("");
+
+console.log(`>>> Check proSession08: `, proSession08); // true
+console.log(`>>> Check pro1Session08: `, pro1Session08); // false
