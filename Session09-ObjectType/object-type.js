75523a777391599c03b58edd2a8c93d8a5abde3b
Loc Ngo-Huynh-Tan <locngohuynhtan@gmail.com>
2025-07-08 07:38:29 +0700

    feat: Session09-ObjectType

---
+++ Session09-ObjectType/object-type.js
@@ -0,0 +1,6 @@
+let person = {};
+person = { name: `Loc` };
+
+person.address = `HCM city`;
+
+console.log(`>>> Check person: `, person);
---
+++ Session09-ObjectType/object-type.ts
@@ -0,0 +1,12 @@
+let personSession09: object;
+personSession09 = { name: `Loc` };
+
+let person01Session09: {};
+
+let person02Session09: {
+  name: string;
+  age: number;
+} = {
+  name: `Loc`,
+  age: 22,
+};
